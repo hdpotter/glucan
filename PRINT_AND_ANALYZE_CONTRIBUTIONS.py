5b3bf038af2d7ef2c2141d7371a69f6849115fac
wildcub57 <cubthatiswild@yahoo.com>
2023-09-16 11:41:52 -0500

    printing blocks' average carbohydrate-ratio-related changes

--- PRINT_AND_ANALYZE_CONTRIBUTIONS.py
+++ PRINT_AND_ANALYZE_CONTRIBUTIONS.py
@@ -85,6 +85,88 @@ def print_and_analyze_block_contributions(block, block_is_a_ratio_block, events,
 			print(contributions_string)		
 
 
+		if block.type == RatioType.CARB_RATIO:
+
+			# calculating the block's average carbohydrate-ratio-related change
+
+			sum_of_negative_changes = 0
+			number_of_calculatable_negative_changes = 0
+			number_of_uncalculatable_negative_changes = 0
+
+			number_of_zero_changes = 0
+
+			sum_of_positive_changes = 0
+			number_of_calculatable_positive_changes = 0
+			number_of_uncalculatable_positive_changes = 0
+
+			for event in events:
+
+				if calculate_contributions(event, block, False)[2] == 1/2:
+
+					if event.start_bg != -1 and event.end_bg != -1:
+
+						change = event.end_bg-event.start_bg
+
+						if change < 0:
+							sum_of_negative_changes += change
+							number_of_calculatable_negative_changes += 1
+
+						elif change == 0:
+							number_of_zero_changes += 1
+
+						elif change > 0:
+							sum_of_positive_changes += change
+							number_of_calculatable_positive_changes += 1
+
+					else:
+
+						if event.end_level == Level.LOW:
+							number_of_uncalculatable_negative_changes += 1
+
+						elif event.end_level == Level.HIGH:
+							number_of_uncalculatable_positive_changes += 1
+
+			if number_of_calculatable_negative_changes != 0:
+				average_negative_changes = sum_of_negative_changes/number_of_calculatable_negative_changes
+			else:
+				average_negative_changes = 0
+				number_of_uncalculatable_negative_changes = 0
+
+			if number_of_calculatable_positive_changes != 0:
+				average_positive_changes = sum_of_positive_changes/number_of_calculatable_positive_changes
+			else:
+				average_positive_changes = 0
+				number_of_uncalculatable_positive_changes = 0
+			
+			sum_of_changes = sum_of_negative_changes + number_of_uncalculatable_negative_changes*average_negative_changes + \
+							 number_of_zero_changes*0 + \
+							sum_of_positive_changes + number_of_uncalculatable_positive_changes*average_positive_changes
+
+			number_of_changes = number_of_calculatable_negative_changes + number_of_uncalculatable_negative_changes + \
+								number_of_zero_changes + \
+								number_of_calculatable_positive_changes + number_of_uncalculatable_positive_changes
+
+			if number_of_changes != 0:
+
+				average_change = sum_of_changes/number_of_changes
+
+				# printing the block's average carbohydrate-ratio-related change
+
+				if block_is_a_ratio_block:
+					change_string = "      "
+				else:
+					change_string = "            "
+
+				change_string = change_string + "=> "
+
+				if average_change > 0:
+					change_string = change_string + "+"
+
+				change_string = change_string + str(average_change) + "mg/dL"
+
+				print(change_string)
+
+			
 		# analyzing the block's contribu...
 
 		fraction_string_exists = False
@@ -141,56 +223,6 @@ def print_and_analyze_block_contributions(block, block_is_a_ratio_block, events,
 		if block.type == RatioType.CARB_RATIO and \
 		   (sufficiency_contributions[Level.LOW][block] >= 1.5 and \
 		    sufficiency_contributions[Level.HIGH][block] >= 1.5):
-			
-				sum_of_negative_changes = 0
-				number_of_calculatable_negative_changes = 0
-				number_of_uncalculatable_negative_changes = 0
-
-				number_of_changes = 0
-
-				sum_of_positive_changes = 0
-				number_of_calculatable_positive_changes = 0
-				number_of_uncalculatable_positive_changes = 0
-
-				for event in events:
-
-					if calculate_contributions(event, block, False)[2] == 1/2:
-
-						if event.start_bg != -1 and event.end_bg != -1:
-
-							change = event.end_bg-event.start_bg
-
-							if change < 0:
-								sum_of_negative_changes += change
-								number_of_calculatable_negative_changes += 1
-
-							elif change == 0:
-								number_of_changes += 1
-
-							elif change > 0:
-								sum_of_positive_changes += change
-								number_of_calculatable_positive_changes += 1
-
-						else:
-
-							if event.end_level == Level.LOW:
-								number_of_uncalculatable_negative_changes += 1
-
-							elif event.end_level == Level.HIGH:
-								number_of_uncalculatable_positive_changes += 1
-
-				average_negative_changes = sum_of_negative_changes/number_of_calculatable_negative_changes
-				average_positive_changes = sum_of_positive_changes/number_of_calculatable_positive_changes
-
-				sum_of_changes = sum_of_negative_changes + number_of_uncalculatable_negative_changes*average_negative_changes + \
-				                 number_of_changes*0 + \
-				                 sum_of_positive_changes + number_of_uncalculatable_positive_changes*average_positive_changes
-
-				number_of_changes = number_of_calculatable_negative_changes + number_of_uncalculatable_negative_changes + \
-				                    number_of_changes + \
-				                    number_of_calculatable_positive_changes + number_of_uncalculatable_positive_changes
-				
-				average_change = sum_of_changes/number_of_changes
 
 				if number_of_uncalculatable_negative_changes < number_of_calculatable_negative_changes and average_change < -25:
 					print("=> Low")
